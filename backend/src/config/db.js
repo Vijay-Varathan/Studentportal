be5563ac6eba2978856172ef8fb72fde26a40039
Vijaykumar V <vijay.v2215@gmail.com>
2026-05-19 13:11:58 +0530

    Update database host in db.js configuration

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,7 @@
 const {Sequelize} = require("sequelize");
 
 const sequelize = new Sequelize("data","vijay","#Vijay1720",{
-    host:"13.201.69.20",
+    host:"13.205.65.232",
     port:3306,
     dialect:"mysql",
     logging:true
